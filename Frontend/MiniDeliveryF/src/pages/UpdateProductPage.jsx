1065bbac1b8e89889ca8f1b3dc75401803fc2f79
Sandel Arache <sandelyulianarachecedano@gmail.com>
2025-11-02 19:56:27 -0400

    Implementación de la actualización de artículos del sistema (Frontend)

---
+++ Frontend/MiniDeliveryF/src/pages/UpdateProductPage.jsx
@@ -0,0 +1,15 @@
+// src/pages/UpdateProductPage.jsx
+import React from "react";
+import ProductUpdateForm from "../components/ProductUpdateForm";
+
+export default function UpdateProductPage() {
+  
+  const productId = 1;
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Gestión de Inventario</h1>
+      <ProductUpdateForm productId={productId} />
+    </div>
+  );
+}
